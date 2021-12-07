c9815cfce299deeb513af576b5e888e4a2dc44fc
qq <bysocket@aliyun.com>
2021-12-07 15:34:51 +0800

    insert : Spring Cloud 之整合 Nacos 实现服务配置中心

---
+++ springcloud-nacos-config-sample/src/main/java/org/spring/springcloud/ConfigApplication.java
@@ -0,0 +1,20 @@
+package org.spring.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * Spring Boot 服务启动类
+ *
+ * Created by bysocket on 21/12/07.
+ */
+@SpringBootApplication  // Spring Boot 应用标识
+public class ConfigApplication {
+
+    public static void main(String[] args) {
+        // 程序启动入口
+        // 启动嵌入式的 Tomcat 并初始化 Spring 环境及其各 Spring 组件
+        SpringApplication.run(ConfigApplication.class,args);
+    }
+}
---
+++ springcloud-nacos-config-sample/src/main/java/org/spring/springcloud/web/ConfigController.java
@@ -0,0 +1,29 @@
+package org.spring.springcloud.web;
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Config 案例
+ * <p>
+ * Created by bysocket on 21/12/06.
+ */
+@RestController
+@Slf4j
+@RefreshScope
+@Data
+public class ConfigController {
+
+    @Value("${blog.name}")
+    private String blogName;
+
+    @GetMapping("/get")
+    public String get() {
+        return "ConfigController#get blog name = " + getBlogName();
+    }
+}
